92e45e819a720a70e33507e6717d075cd92849a3
André Otávio <49661887+dequim1000@users.noreply.github.com>
2020-12-09 10:26:24 -0300

    Update Usuario.php

--- controllers/Usuario.php
+++ controllers/Usuario.php
@@ -1,16 +1,18 @@
 <?php
+
 header('Content-type: application/json');
 header('Access-Control-Allow-Origin: *');
 include '../models/Client_model.php';
-$user=$_POST['user'];
-$pass=$_POST['pass'];
+    $user=$_POST['user'];
+    $pass=$_POST['pass'];
 
 $usuario = new Client($user,'');
-if($user==$usuario->getEmail() && $pass ==$usuario->getPasswordClient()){
-    $resposta = array('resposta'=>'1');
-}else{
-    $resposta = array('resposta'=>'0');
-}
+
+    if($user==$usuario->getEmail() && $pass ==$usuario->getPasswordClient()){
+        $resposta = array('resposta'=>'1');
+    }else{
+        $resposta = array('resposta'=>'0');
+    }
 
 ob_clean();
 echo json_encode($resposta);
