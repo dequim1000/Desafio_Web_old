e99914e90a95c8e8304fcb1f1f291f6bc09d7623
André Otávio <49661887+dequim1000@users.noreply.github.com>
2020-12-09 09:39:42 -0300

    Update DetailsPlanCall

--- models/DetailsPlanCall_model.php
+++ models/DetailsPlanCall_model.php
@@ -6,23 +6,32 @@ require_once 'Client_model.php';
 
 
 
-class DetailsSms{
+class DetailsCall{
     private $id;
     private $clientId;
     private $planId;
     private $dtRequest;
+    private $priceCall;
     private $url;
+    private $body;
+    private $responseStatus;
+    private $responseBody;
+    private $postActions;
     private $requestPlan;
     private $restantePlan;
+    private $extrasPlan;
 
-    public function fabricaDetailsSms($id,$clientId,$planId,$dtRequest,$url,$requestPlan,$restantePlan){
+    public function fabricaDetailsCall($id,$clientId,$planId,$dtRequest,$url,$priceCall,$requestPlan,$restantePlan,$extrasPlan){
         $this->id=$id;
         $this->clientId=$clientId;
         $this->planId=$planId;
         $this->dtRequest=$dtRequest;
         $this->url=$url;
+        $this->priceCall=$priceCall;  
         $this->requestPlan=$requestPlan;
         $this->restantePlan=$restantePlan;
+        $this->extrasPlan = $extrasPlan;
+       
     }
 
     public function getId(){
@@ -43,63 +52,32 @@ class DetailsSms{
     public function getUrl(){
         return $this->url;
     }
+    public function getPriceCall(){
+        return $this->priceCall;
+    }
+    public function getBody(){
+        return $this->body;
+    }
+    public function getStatus(){
+        return $this->responseStatus;
+    }
     public function getRequest(){
         return $this->requestPlan;
     }
     public function getRestantes(){
         return $this->restantePlan;
     }
-    
-    public function trasDoBancoSMS($planid, $clientid){
-
-            $SQLTipo = "SELECT cp.ClientId as 'CLIENTID'
-                , PLA.ID AS 'PLANOID'
-                , PLA.NAME AS 'PLANONAME'
-                , PLA.PRICE AS 'PRICE'
-                , PLA.REQUESTQUANTITY AS 'REQUESTQUANTITY'
-                , CP.CLIENTID AS 'CLIENTID'
-                , cp.SMSCredits AS 'SMSCONTRATADOS'
-                , count(CPR.id) as 'UTILIZADOS'
-                , CPR.ID AS 'IDAPIREQUEST'
-                , CPR.URL AS 'URL'
-                , CPR.BODY AS 'BODY'
-                , CPR.RESPONSESTATUS AS 'RESPONSESTATUS'
-                , CPR.RESPONSEBODY AS 'RESPONSEBODY'
-                , CPR.POSTACTIONS AS 'POSTACTIONS'
-                , (cp.SMSCredits - count(CPR.id)) as 'RESTANTES'
-                , MONTH(DtRequest) as 'DTREQUEST'
-                    FROM ClientPlan as CP
-                    JOIN ClientApiRequest as CPR 
-                    on cp.ClientId = CPR.ClientId
-                    JOIN PLANX AS PLA
-                    ON PLA.ID = '$planid'
-                    WHERE CPR.ClientId  = '$clientid'
-                    AND CPR.URL LIKE '%/api/sms/send%'
-                    AND CPR.RESPONSESTATUS = 200
-                    and Month(CPR.DtRequest) = month(sysdate())
-                    group by CPR.PlanId;";
-
-        try {
-            $conexao = new Conexao();
-            $consulta = $conexao->conecta()->query("".$SQLTipo);
-            $ret=array();
-                foreach($consulta as $api){
-                    $planx = new PlanX();
-                    $planx->fabricaPlanX($api['PLANOID'],$api['PLANONAME'],$api['REQUESTQUANTITY'],$api['PRICE']);
-                    $clientPlan = new ClientPlan();
-                    $clientPlan->fabricaClientPlan($api['CLIENTID'], $planx, $api['SMSCONTRATADOS']);
-                    $cliApi = new Apirequest();
-                    $cliApi->fabricaApirequest($api['IDAPIREQUEST'],$api['CLIENTID'],$planx, $api['DTREQUEST'],$api['URL'],$api[''],$api['BODY'],$api['RESPONSESTATUS'],$api['UTILIZADOS'],$api['RESTANTES'],$api[''], $api['RESPONSEBODY'],$api['POSTACTIONS'] );
-                    array_push($ret,$cliApi );
-                }
-
-                return $ret;
-        } catch (Exception $e) {
-            return $e;
-        }
+    public function getExtras(){
+        return $this->extrasPlan;
     }
-
-public function trasDoBancoCALL($planid, $clientid){
+    public function getResponsebody(){
+        return $this->responseBody;
+    }
+    public function getPost(){
+        return $this->postActions;
+    }
+    
+   public function trasDoBancoCALL($planid, $clientid, $mesSelect){
 
 	            $SQLTipo = " SELECT C.NAME AS 'CLIENTE'
             , PLA.ID AS 'PLANOID'
@@ -131,7 +109,7 @@ public function trasDoBancoCALL($planid, $clientid){
         WHERE CPR.CLIENTID = '$clientid'
         AND CPR.URL LIKE '%/api/call/send%'
         AND CPR.RESPONSESTATUS = 200
-        AND MONTH(CPR.DTREQUEST) = MONTH(SYSDATE())
+        AND MONTH(CPR.DTREQUEST) = MONTH('$mesSelect')
     GROUP BY CPR.PLANID;";
 
  	try {
@@ -143,8 +121,8 @@ public function trasDoBancoCALL($planid, $clientid){
                     $planx->fabricaPlanX($api['PLANOID'],$api['PLANONAME'],$api['REQUESTQUANTITY'],$api['PRICE']);
                     $clientPlan = new ClientPlan();
                     $clientPlan->fabricaClientPlan($api['CLIENTID'], $planx, $api['SMSCONTRATADOS']);
-                    $cliApi = new Apirequest();
-                    $cliApi->fabricaApirequest($api['IDAPIREQUEST'],$api['CLIENTID'],$planx, $api['DTREQUEST'],$api['URL'],$api['UTIQUANTITY'],$api['BODY'],$api['RESPONSESTATUS'],$api['UTILIZADOS'],$api['RESTANTES'],$api['EXTRAS'],$api['RESPONSEBODY'],$api['POSTACTIONS'] );
+                    $cliApi = new DetailsCall();
+                    $cliApi->fabricaDetailsCall($api['IDAPIREQUEST'],$api['CLIENTID'],$planx, $api['DTREQUEST'],$api['URL'],$api['UTIQUANTITY'],$api['BODY'],$api['UTILIZADOS'],$api['RESTANTES'],$api['EXTRAS']);
                     array_push($ret,$cliApi );
                 }
 
